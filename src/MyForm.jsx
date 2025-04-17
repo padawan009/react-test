b7571154d9a93535f1869bd37e108a82123cca9b
Asya I <as.izmaylova@gmail.com>
2025-04-17 03:39:41 +0300

    added some code

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './MyForm.css'
 
 function MyForm() {
+
+    const[formData, setFormData] = useState({
+        firstName: "",
+        surname: "",
+        email: "",
+        password: "",
+    });
+
+    const[formErrors, setFormErrors] = useState({});
+    const[isSubmit, setIsSubmit] = useState(false)
+
+    function handleInput(e) {
+        const { name, value } = e.target;
+        setFormData(prevData => ({...prevData, [name]: value }))
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setFormErrors(validateForm(formData));
+        setIsSubmit(true);
+    }
+
+    useEffect(() => {
+        console.log(formErrors)
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+            console.log(formData);
+        }
+    },[formErrors])
+
+    function validateForm(values) {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if(!values.firstName) {
+            errors.firstName = 'Name is required!';
+        }
+        if(!values.surname) {
+            errors.surname = 'Surname is required!';
+        }
+        if(!values.email) {
+            errors.email = 'Email is required!';
+        }
+        if(!values.password) {
+            errors.password = 'Password is required!';
+        }
+        return errors;
+    }
+
   return (
     <div className='my-form'>
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <div>
                 <h2>Welcome,</h2>
                 <p>sing up to continue</p>   
@@ -12,25 +59,49 @@ function MyForm() {
 
             <label htmlFor="firstName">
                 Name:
-            <input type="text" name="firstName" id="firstName" /> 
+            <input 
+            type="text" 
+            name="firstName" 
+            id="firstName"
+            value={formData.firstName}
+            onChange={handleInput} /> 
             </label>
-            
+            <p>{formErrors.firstName}</p>
+
             <label htmlFor="surname">
                 Surname:
-                <input type="text" name="surname" id="surname" />
+                <input 
+                type="text" 
+                name="surname" 
+                id="surname"
+                value={formData.surname}
+                onChange={handleInput} />
             </label>
+            <p>{formErrors.surname}</p>
 
             <label htmlFor="email">
                 E-mail:
-                <input type="email" name="email" id="email"/>
+                <input 
+                type="email" 
+                name="email" 
+                id="email"
+                value={formData.email}
+                onChange={handleInput}/>
             </label>
+            <p>{formErrors.email}</p>
 
             <label htmlFor="password">
                 Password:
-                <input type="password" name="password" id="password"/>
+                <input 
+                type="password" 
+                name="password" 
+                id="password"
+                value={formData.password}
+                onChange={handleInput}/>
             </label>
+            <p>{formErrors.password}</p>
 
-            <button type="submit">Sign Up</button>    
+            <button type="button">Sign Up</button>    
         </form>
 
     </div>
